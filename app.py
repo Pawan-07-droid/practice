255206238b79673ee033cf4d2b8425878d3031e4
Pawan-07-droid <pawan.cse07@gmail.com>
2025-06-07 22:54:01 +0530

    developera story

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ def addition(a,b):
 
 def sub(a,b):
     return abs(a-b)
+
+# (venv) PS D:\git> git checkout developerA
+# M       app.py
+# Switched to branch 'developerA'
+
+def div(a,b):
+    return a/b
